b320168cc432b7d48756c25be4486eac297116d5
antoniofpereir <antoniofpereir@gmail.com>
2019-03-05 22:53:07 +0000

    Changes for presentation

--- src/containers/PokemonHome/index.js
+++ src/containers/PokemonHome/index.js
@@ -21,10 +21,6 @@ class PokemonHome extends React.Component {
     }
   }
 
-  // incrementInContext = () => {
-  //   this.context.execute('RANDOM_INCREMENT');
-  // }
-
   increment = () => {
     this.setState({
       value: this.state.value + 1,
@@ -33,20 +29,24 @@ class PokemonHome extends React.Component {
 
   // incrementBy3 = () => {
   //   this.setState((prevState) => {
-  //     return {value: prevState.value + 1}
+  //     return { value: prevState.value + 1 }
   //   });
   //   this.setState((prevState) => {
-  //     return {value: prevState.value + 1}
+  //     return { value: prevState.value + 1 }
   //   });
   //   this.setState((prevState) => {
-  //     return {value: prevState.value + 1}
+  //     return { value: prevState.value + 1 }
   //   });
+  // }
+
+  // incrementInContext = () => {
+  //   this.context.execute('RANDOM_INCREMENT');
   // }
 
   render() {
     return (
       <div style={styles.container} >
-        <img onClick={this.increment} src="https://res.cloudinary.com/teepublic/image/private/s--LMs-pmHt--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1515387356/production/designs/2262554_1.jpg" alt="Pokémon Logo" height="600" width="600" />
+        <img onClick={this.increment} src="https://res.cloudinary.com/teepublic/image/private/s--LMs-pmHt--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1515387356/production/designs/2262554_1.jpg" alt="Pokémon Logo" height="300" width="300" />
       </div>
     );
   }
